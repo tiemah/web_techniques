2cad80b3a2c450477b06c7a7da6b5cfd654f6c90
tiemah <doreentiema87@gmail.com>
2023-11-17 07:02:29 +0300

    asynchronous programming

---
+++ asynchronous_programming/xmlhttprequest.js
@@ -0,0 +1,26 @@
+const xhrAPI = new XMLHttpRequest(); //Creating an object of the API
+
+
+
+// Adding event listeners
+
+xhrAPI.addEventListener('load', function(){
+    if(xhrAPI.status === 200){ //checks if the request was successful
+        console.log("Request was successful:", xhrAPI.responseText);
+
+}
+else{
+    console.error("Error:", xhrAPI.statusText); //handles errors r other HTTP ststus error codes
+}
+});
+
+// Event listener for the "error" event (if the request encounters an error)
+xhrAPI.addEventListener('error', function () {
+    console.error('Request encountered an error.');
+});
+
+//Specifying that we are making a GET request to the server
+xhrAPI.open('GET', 'https://jsonplaceholder.typicode.com/', true); //Used JSONPlaceholder url for testing
+
+// Invoking the asynchronous operation using the send method
+xhrAPI.send();
