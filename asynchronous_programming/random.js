235e99a1c5388590ffe5c783686f8c7d4bfd0efb
Doreen Tiema <doreentiema87@gmail.com>
2024-03-04 06:42:38 +0300

    making the function asynchronous by using the setTimeout method to introduce the asynchronous behaviour

--- asynchronous_programming/random.js
+++ asynchronous_programming/random.js
@@ -1,8 +1,25 @@
+// function getRandomDelay(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
+  
+//   async function executeWithRandomDelay() {
+//     const minDelay = 1000; // Minimum delay time in milliseconds
+//     const maxDelay = 5000; // Maximum delay time in milliseconds
+  
+//     const delayTime = getRandomDelay(minDelay, maxDelay);
+  
+//     setTimeout(() => {
+//       console.log(`Code executed after ${delayTime} milliseconds.`);
+//     }, delayTime);
+//   }
+  
+//   executeWithRandomDelay();
+  
 function getRandomDelay(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
-  async function executeWithRandomDelay() {
+  function executeWithRandomDelay(callback) {
     const minDelay = 1000; // Minimum delay time in milliseconds
     const maxDelay = 5000; // Maximum delay time in milliseconds
   
@@ -10,8 +27,12 @@ function getRandomDelay(min, max) {
   
     setTimeout(() => {
       console.log(`Code executed after ${delayTime} milliseconds.`);
+      callback();
     }, delayTime);
   }
   
-  executeWithRandomDelay();
+  // Usage
+  executeWithRandomDelay(() => {
+    console.log("Callback function executed.");
+  });
   
